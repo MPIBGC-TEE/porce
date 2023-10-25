b18637310e569c2315b6acb2a2de1003863ca7de
csierra <csierra@bgc-jena.mpg.de>
2023-10-25 17:25:38 +0200

    Created html report from R script using rmarkdown::render

--- simulation/ATTO/transient_evolvingGPP.R
+++ simulation/ATTO/transient_evolvingGPP.R
@@ -1,22 +1,38 @@
+#' ---
+#' title: "Transient simulations of respiration with time dependent GPP"
+#' author: Carlos A. Sierra
+#' output: html_document
+#' ---
+
+#' This document presents an example for the computation of respiration fluxes
+#' using time-dependent values of GPP for ATTO. For this, we first obtain a 
+#' time series of typical values of GPP repeated for 10 years on a monthly basis.
+#' 
+#' To get started we first call necessary libraries and data
+
 library(porce)
 library(forecast)
 library(SoilR)
 
 setwd("~/MPIBGC-TEE/porce/simulation/ATTO/")
 monthly_gpp<-read.csv("inputs/ATTO_INST_monthlyGPP_2014_2019.csv")
-gpp_ts<-lapply(X=as.list(monthly_gpp[-1]), FUN=ts, start=c(2014,1), frequency = 12)
-gpp_ets<-lapply(X=gpp_ts, FUN=ets)
 pool_names<-c("Foliage", "Wood", "Fine roots", "Coarse roots", "Fine litter", "CWD", "Soil (0-30 cm)")
 
-lapply(gpp_ets, plot)
+#' We fit an ETS (error, trend, seasonality) model to the time series, make a forecast, and compute
+#' the mean value of the forecast. This results in a stationary series based on the original observed
+#' series.
+gpp_ts<-lapply(X=as.list(monthly_gpp[-1]), FUN=ts, start=c(2014,1), frequency = 12)
+gpp_ets<-lapply(X=gpp_ts, FUN=ets)
+#lapply(gpp_ets, plot)
 ny<-10; freq<-12
-
 gpp_forecast<-lapply(gpp_ets, FUN=forecast, h=ny*freq)
 gpp_sim<-sapply(gpp_forecast, function(x) x$mean)
 
+#' We can see now the mean forecast, which can be used as toy series to run simulations.
 years<-seq(1/freq, ny, by=1/freq)
 matplot(years, gpp_sim, type="l", lty=1, col=1:3, bty="n")
 
+#' Let's compute now equilibrium stocks for one single parameter set and run a simulation with it.
 xss=equilibriumStock(lam(input=inputGPP(mean(gpp_sim[,1])), matrix=makeB(modpars[1,])))
 
 influxFunc<-function(t, series=data.frame(years,gpp_sim[,1])){
@@ -25,9 +41,9 @@ influxFunc<-function(t, series=data.frame(years,gpp_sim[,1])){
 }
 
 mod<-Model(t=years,A=makeB(modpars[1,]),ivList = as.numeric(xss), inputFluxes = influxFunc)
-Ct<-getC(mod)
 Rt<-getReleaseFlux(mod)
 
+#' This can be aggregated in autotrophic and heterotrophic pools
 autotroph_pools=match(c("Foliage", "Wood", "Fine roots", "Coarse roots"), table=pool_names)
 heterotroph_pools=match(c("Fine litter", "CWD", "Soil (0-30 cm)"), table=pool_names)
 
@@ -37,6 +53,8 @@ lines(years, rowSums(Rt[,autotroph_pools]),col=4)
 lines(years, rowSums(Rt[,heterotroph_pools]),col=5)
 legend("topright", c("Gross primary production", "Ecosystem respiration",
                      "Autotrophic respiration", "Heterotrophic respiration"), lty=1, col=c(3,2,4,5),bty="n")
+
+#' Because we have 1000 parameter sets in modpars, we can run now simulations in parallel for sets
 
 library(parallel)
 no_cores <- detectCores() - 2
@@ -62,6 +80,7 @@ HetResp<-sapply(respList, FUN=function(x) rowSums(x[,heterotroph_pools]))
 meanHR<-apply(X=HetResp, MARGIN=1, FUN=mean)
 sdHR<-apply(X=HetResp, MARGIN=1, FUN=sd)
 
+#' Here are the results presented as uncertainty in respiration given uncertainty in parameters
 plot(years, gpp_sim[,1], type="l", ylab=expression(paste("Flux (Mg C h", a^-1, " y", r^-1, ")")), ylim=c(0,400), col=3, bty="n")
 polygon(x=c(years,rev(years)), y=c(meanER+sdER, rev(meanER-sdER)), col=2, border=NA)
 polygon(x=c(years,rev(years)), y=c(meanAR+sdAR, rev(meanAR-sdAR)), col=4, border=NA)
